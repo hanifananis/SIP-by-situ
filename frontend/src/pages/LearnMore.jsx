b7b2cf7ea853172ff8b074e0199a534016674681
RafaUlinnuha <ulinnuharafaazka@gmail.com>
2023-10-30 20:15:53 +0700

    feat: heading

---
+++ frontend/src/pages/LearnMore.jsx
@@ -0,0 +1,18 @@
+import { Flex, Heading } from '@chakra-ui/react'
+import React from 'react'
+
+const LearnMore = () => {
+  return (
+    <Flex>
+        <Heading
+            alignItems={'center'}
+            mx={'auto'}
+            paddingY={28}
+            fontSize={48} 
+            fontWeight={'bold'}
+        >Pelajari Pemilu 2024</Heading>
+    </Flex>
+  )
+}
+
+export default LearnMore
