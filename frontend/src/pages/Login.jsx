2bf301dd40ea7d975be803a85814047081280b7e
RafaUlinnuha <ulinnuharafaazka@gmail.com>
2023-11-14 16:29:25 +0700

    update toaster

--- frontend/src/pages/Login.jsx
+++ frontend/src/pages/Login.jsx
@@ -39,7 +39,16 @@ export default function Login() {
       password: values.password
     })
     .then(() => {
-      toast.success('Login successfully');
+      toast.success('Login berhasil', {
+        position: "top-right",
+        autoClose: 5000,
+        hideProgressBar: false,
+        closeOnClick: true,
+        pauseOnHover: true,
+        draggable: true,
+        progress: undefined,
+        theme: "colored",
+        });
       navigate('/');
     })
     .catch((error) => {
