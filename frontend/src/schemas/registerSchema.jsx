57a7592fed72a888bb54143fa8edf03aac4a818c
RafaUlinnuha <ulinnuharafaazka@gmail.com>
2023-11-14 10:15:57 +0700

    register with formik yup

--- frontend/src/schemas/registerSchema.jsx
+++ frontend/src/schemas/registerSchema.jsx
@@ -2,14 +2,23 @@ import * as yup from 'yup'
 
 const passwordRules = /^(?=.*\d)(?=.*[a-z])(?=.*[A-Z])(?=.*[a-zA-Z]).{8,}$/;
 
-export const loginSchema = yup.object().shape({
+export const registerSchema = yup.object().shape({
+    name: yup
+        .string()
+        .required("Required"),
     email: yup
         .string()
         .email("Please enter a valid email")
+        .required("Required"),
+    no_telp: yup
+        .string()
         .required("Required"),
     password: yup
         .string()
         .min(5)
         .matches(passwordRules, {message: "Please create a stronger password"})
         .required("Required"),
+    confirmPassword: yup
+        .string()
+        .oneOf([yup.ref('password'), null], "Passwords must match")
 });
