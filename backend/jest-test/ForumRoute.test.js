9967b6825cd25b0ff7d1d91f929afa012aa55713
hanifananis <hanifan20001@mail.unpad.ac.id>
2023-12-16 22:25:48 +0700

    update testing

--- backend/jest-test/ForumRoute.test.js
+++ backend/jest-test/ForumRoute.test.js
@@ -2,50 +2,179 @@ import request from 'supertest';
 import app from '../index.js';
 
 const forumAPI = describe('Forum API', () => {
-    // it('should get all forum', async () => {
-    //     const response = await request(app).get('/forums')
-    //     expect(response.status).toBe(200)
-    //     expect(response.body).toEqual([
-    //         {
-    //             "_id": "65733b6003cfe82c579c8ba7",
-    //             "judul": "Forum 1",
-    //             "isi": "Isi dari forum pertama",
-    //             "penulis_id": "65521297ffe27231eab2ce59",
-    //             "createdAt": "2023-12-08T15:50:56.038Z",
-    //             "updatedAt": "2023-12-08T15:50:56.038Z",
-    //             "__v": 0,
-    //             "penulis": {
-    //                 "name": "John Doe",
-    //                 "roles": "user"
-    //             }
-    //         },
-    //         {
-    //             "_id": "65733b6303cfe82c579c8bc4",
-    //             "judul": "Forum 2",
-    //             "isi": "Isi dari forum kedua",
-    //             "penulis_id": "655335e28b2caff59cfbe499",
-    //             "createdAt": "2023-12-08T15:50:59.090Z",
-    //             "updatedAt": "2023-12-08T15:50:59.090Z",
-    //             "__v": 0,
-    //             "penulis": {
-    //                 "name": "pedes",
-    //                 "roles": "user"
-    //             }
-    //         }
-    //     ])
-    // })
+    it('should get all forum', async () => {
+        const response = await request(app).get('/forums')
+        expect(response.status).toBe(200)
+        expect(response.body).toEqual([
+            {
+                "_id": "65733b6003cfe82c579c8ba7",
+                "judul": "Forum 1",
+                "isi": "Isi dari forum pertama",
+                "penulis_id": "65521297ffe27231eab2ce59",
+                "createdAt": "2023-12-08T15:50:56.038Z",
+                "updatedAt": "2023-12-08T15:50:56.038Z",
+                "__v": 0,
+                "penulis": {
+                    "name": "John Doe",
+                    "roles": "user"
+                }
+            },
+            {
+                "_id": "65733b6303cfe82c579c8bc4",
+                "judul": "Forum 2",
+                "isi": "Isi dari forum kedua",
+                "penulis_id": "655335e28b2caff59cfbe499",
+                "createdAt": "2023-12-08T15:50:59.090Z",
+                "updatedAt": "2023-12-08T15:50:59.090Z",
+                "__v": 0,
+                "penulis": {
+                    "name": "pedes",
+                    "roles": "user"
+                }
+            }
+        ])
+    })
 
-    // it('should get a forum by ID', async () => {
-    //     const response = await request(app).get('/forums/:forumId')
-    //     expect(response.status).toBe(200)
-    //     expect(response.body).toEqual(/* body */)
-    // })
+    it('should get a forum by ID', async () => {
+        const forumId = "65733b6003cfe82c579c8ba7";
+        const response = await request(app).get(`/forums/${forumId}`)
+        expect(response.status).toBe(200)
+        expect(response.body).toEqual(
+            {
+                "_id": "65733b6003cfe82c579c8ba7",
+                "judul": "Forum 1",
+                "isi": "Isi dari forum pertama",
+                "penulis_id": "65521297ffe27231eab2ce59",
+                "createdAt": "2023-12-08T15:50:56.038Z",
+                "updatedAt": "2023-12-08T15:50:56.038Z",
+                "__v": 0,
+                "penulis": {
+                    "name": "John Doe",
+                    "roles": "user"
+                },
+                "comments": [
+                    {
+                        "_id": "65733b6003cfe82c579c8ba9",
+                        "penulis_id": "65521297ffe27231eab2ce59",
+                        "forum_id": "65733b6003cfe82c579c8ba7",
+                        "content": "Comment 1 on Forum 1",
+                        "replies": [
+                            {
+                                "penulis_id": "65521297ffe27231eab2ce59",
+                                "content": "Reply 1 on Comment 1 in Forum 1",
+                                "_id": "65733b6003cfe82c579c8bab",
+                                "created_at": "2023-12-08T15:50:56.637Z",
+                                "user": {
+                                    "name": "John Doe",
+                                    "roles": "user"
+                                }
+                            },
+                            {
+                                "penulis_id": "65521297ffe27231eab2ce59",
+                                "content": "Reply 2 on Comment 1 in Forum 1",
+                                "_id": "65733b6003cfe82c579c8bae",
+                                "created_at": "2023-12-08T15:50:56.939Z",
+                                "user": {
+                                    "name": "John Doe",
+                                    "roles": "user"
+                                }
+                            },
+                            {
+                                "penulis_id": "6553346c8b2caff59cfbe487",
+                                "content": "Updated reply content",
+                                "_id": "65733baac786af31a3d5dc04",
+                                "created_at": "2023-12-08T15:52:10.054Z",
+                                "user": {
+                                    "name": "kopi",
+                                    "roles": "user"
+                                }
+                            }
+                        ],
+                        "created_at": "2023-12-08T15:50:56.290Z",
+                        "__v": 1,
+                        "user": {
+                            "name": "John Doe",
+                            "roles": "user"
+                        }
+                    },
+                    {
+                        "_id": "65733b6103cfe82c579c8bb2",
+                        "penulis_id": "65521297ffe27231eab2ce59",
+                        "forum_id": "65733b6003cfe82c579c8ba7",
+                        "content": "Comment 2 on Forum 1",
+                        "replies": [
+                            {
+                                "penulis_id": "65521297ffe27231eab2ce59",
+                                "content": "Reply 1 on Comment 2 in Forum 1",
+                                "_id": "65733b6103cfe82c579c8bb4",
+                                "created_at": "2023-12-08T15:50:57.555Z",
+                                "user": {
+                                    "name": "John Doe",
+                                    "roles": "user"
+                                }
+                            },
+                            {
+                                "penulis_id": "65521297ffe27231eab2ce59",
+                                "content": "Reply 2 on Comment 2 in Forum 1",
+                                "_id": "65733b6103cfe82c579c8bb7",
+                                "created_at": "2023-12-08T15:50:57.860Z",
+                                "user": {
+                                    "name": "John Doe",
+                                    "roles": "user"
+                                }
+                            }
+                        ],
+                        "created_at": "2023-12-08T15:50:57.241Z",
+                        "__v": 0,
+                        "user": {
+                            "name": "John Doe",
+                            "roles": "user"
+                        }
+                    },
+                    {
+                        "_id": "65733b6203cfe82c579c8bbb",
+                        "penulis_id": "65521297ffe27231eab2ce59",
+                        "forum_id": "65733b6003cfe82c579c8ba7",
+                        "content": "Comment 3 on Forum 1",
+                        "replies": [
+                            {
+                                "penulis_id": "65521297ffe27231eab2ce59",
+                                "content": "Reply 1 on Comment 3 in Forum 1",
+                                "_id": "65733b6203cfe82c579c8bbd",
+                                "created_at": "2023-12-08T15:50:58.419Z",
+                                "user": {
+                                    "name": "John Doe",
+                                    "roles": "user"
+                                }
+                            },
+                            {
+                                "penulis_id": "65521297ffe27231eab2ce59",
+                                "content": "Reply 2 on Comment 3 in Forum 1",
+                                "_id": "65733b6203cfe82c579c8bc0",
+                                "created_at": "2023-12-08T15:50:58.781Z",
+                                "user": {
+                                    "name": "John Doe",
+                                    "roles": "user"
+                                }
+                            }
+                        ],
+                        "created_at": "2023-12-08T15:50:58.167Z",
+                        "__v": 0,
+                        "user": {
+                            "name": "John Doe",
+                            "roles": "user"
+                        }
+                    }
+                ]
+            }
+        )
+    })
 
     it('should create a new forum', async () => {
         const newForum = {
             "judul": "Test Forum Baru",
             "isi": "Isi dari test forum baru",
-            "penulisId": "6552140effe27231eab2ce63" // jane doe
+            "penulis_id": "6552140effe27231eab2ce63" // jane doe
         }
         const response = await request(app)
             .post('/forums')
@@ -57,75 +186,105 @@ const forumAPI = describe('Forum API', () => {
         }))
     })
 
-    // it('should update a forum by ID', async () => {
-    //     const updateForum = {
-    //         "judul": "Update Forum 2",
-    //         "isi": "update isi forum 2",
-    //     }
-    //     const response = await request(app)
-    //         .put('/forums/:forumId')
-    //         .send(updateForum)
+    it('should update a forum by ID', async () => {
+        const forumId = "6576d6f02e5da0f045352ee6"
+        const updateForum = {
+            "judul": "Delete Forum 2",
+            "isi": "Delete isi forum 2",
+        }
+        const response = await request(app)
+            .put(`/forums/${forumId}`)
+            .send(updateForum)
         
-    //     expect(response.status).toBe(200)
-    //     expect(response.body).toEqual(expect.objectContaining({
-    //         "judul": expect.stringContaining("Update Forum 2")
-    //     }))
-    // })
+        expect(response.status).toBe(200)
+        expect(response.body).toEqual(expect.objectContaining({
+            "judul": expect.stringContaining("Delete Forum 2")
+        }))
+    })
 
-    // it('should delete a forum by ID', async () => {
-    //     const response = await request(app).delete('/forums/:forumId')
-    //     expect(response.status).toBe(200)
-    // })
+    it('should delete a forum by ID', async () => {
+        const forumId = "6576d51ca570868e2063be8a"
+        const response = await request(app).delete(`/forums/${forumId}`)
+        expect(response.status).toBe(200)
+    })
 })
 
-// const commentAPI = describe('Comment API', () => {
-//     it('should create a new comment', async () => {
-//         const newComment = {
-//             "forumId": "65573ed435c10337d5fa5ea7",
-//             "userId": "655331c329e026ca0e96885f",
-//             "content": "This is a sample comment."
-//         }
-//         const response = await request(app)
-//             .post('/')
-//             .send(newComment)
+const commentAPI = describe('Comment API', () => {
+    it('should create a new comment', async () => {
+        const newComment = {
+            "forum_id": "6576dabb8c6cc8f7a8b2f9d7",
+            "penulis_id": "6552140effe27231eab2ce63",
+            "content": "test comment"
+        }
+        const response = await request(app)
+            .post('/comments')
+            .send(newComment)
 
-//         expect(response.status).toBe(201)
-//         expect(response.body).toEqual(/** body */)
-//     })
+        expect(response.status).toBe(201)
+        expect(response.body).toEqual(expect.objectContaining({
+            "content": expect.stringContaining("test comment")
+        }))
+    })
 
-//     it('should update a comment by ID', async () => {
-//         const updateComment = {
-//             "forumId": "65573ed435c10337d5fa5ea7",
-//             "commentId": "655749883e4aa8469cb90021",
-//             "content": "Updated comment content."
-//         }
-//         const response = await request(app)
-//             .put('/')
-//             .send(updateComment)
+    it('should update a comment by ID', async () => {
+        const comment_id = "6576db72dec6b46f47c1faa3"
+        const updateComment = {
+            "content": "Test Update comment content"
+        }
+        const response = await request(app)
+            .put(`/comments/${comment_id}`)
+            .send(updateComment)
 
-//         expect(response.status).toBe(200)
-//         expect(response.body).toEqual(/** body */)
-//     })
+        expect(response.status).toBe(200)
+        expect(response.body).toEqual(expect.objectContaining({
+            "content": expect.stringContaining("Test Update comment content")
+        }))
+    })
 
-//     it('should delete a comment by ID', async () => {
-//         const response = await request(app).delete('/comments/:forumId/:commentId')
-//         expect(response.status).toBe(200)
-//     })
-// })
+    it('should delete a comment by ID', async () => {
+        const comment_id = "6576db72dec6b46f47c1faa3"
+        const response = await request(app).delete(`/comments/${comment_id}`)
+        expect(response.status).toBe(200)
+    })
+})
 
-// const replyAPI = describe('Reply API', () => {
-//     it('should add reply of a comment', async () => {
-//         const addReply = {
-//             "forumId": "65573ed435c10337d5fa5ea7",
-//             "commentId": "65573ed435c10337d5fa5ea8",
-//             "userId": "655331c329e026ca0e96885f",
-//             "content": "This is a sample reply."
-//         }
-//         const response = await request(app)
-//             .post('/replies')
-//             .send(addReply)
+const replyAPI = describe('Reply API', () => {
+    it('should add reply of a comment', async () => {
+        const comment_id = "657728539011896d9bba068d"
+        const addReply = {
+            "penulis_id": "6552140effe27231eab2ce63",
+            "content": "test reply comment"
+        }
+        const response = await request(app)
+            .post(`/comments/${comment_id}/replies`)
+            .send(addReply)
 
-//         expect(response.status).toBe(201)
-//         expect(response.body).toEqual(/** body */)
-//     })
-// })
+        expect(response.status).toBe(201)
+        expect(response.body).toEqual(expect.objectContaining({
+            "content": expect.stringContaining("test reply comment")
+        }))
+    })
+
+    it('should update reply of a comment', async () => {
+        const comment_id = "657728539011896d9bba068d"
+        const replyId = "657729c02ad0243107688e90"
+        const updateReply = {
+            "content": "Updated reply content"
+        }
+        const response = await request(app)
+            .put(`/comments/${comment_id}/${replyId}`)
+            .send(updateReply)
+        
+        expect(response.status).toBe(200)
+        expect(response.body).toEqual(expect.objectContaining({
+            "content": expect.stringContaining("Updated reply content")
+        }))
+    });
+
+    it('should delete reply of a comment', async () => {
+        const comment_id = "657728539011896d9bba068d"
+        const replyId = "657729c02ad0243107688e90"
+        const response = await request(app).del(`/comments/${comment_id}/${replyId}`)
+        expect(response.status).toBe(200)
+    });
+})
--- frontend/cypress/e2e/SIP-testing.cy.js
+++ frontend/cypress/e2e/SIP-testing.cy.js
@@ -1,4 +1,4 @@
-// mengakses URL
+// mengakses URL website SIP
 beforeEach(() => {
     cy.viewport(1920, 1080);
     cy.visit('http://localhost:5173');
@@ -10,79 +10,141 @@ beforeEach(() => {
 });
 
 // validasi isi konten dalam Landing Page
-describe('Landing Page', () => {
-    it('successfully accessed Landing Page', () => {
-        cy.contains('Partai Politik');
-        cy.contains('Pelajari Pemilu 2024');
-        cy.contains('Capres dan Cawapres 2024');
-        cy.contains('Kilas Balik Pemilu 2019');
-    });
-});
+// describe('Landing Page', () => {
+//     it('successfully accessed Landing Page', () => {
+//         cy.contains('Kenali Proses Pemilu dengan Lebih Dekat');
+//         cy.contains('Ayo Bergabung di Forum Kami').click();
+//         cy.contains('Partai Politik');
+//     });
+
+//     it('successfully accessed Pelajari Pemilu 2024', () => {
+//         cy.contains('Pelajari Pemilu 2024');
+//         cy.get('a:contains("Klik untuk membaca")').eq(0).click();
+//         cy.go('back');
+//     });
+
+//     it('successfully accessed Capres dan Cawapres 2024', () => {
+//         cy.contains('Capres dan Cawapres 2024');
+
+//         // open card Calon 1
+//         cy.get('a:contains("Lihat Profil Calon")').eq(2).click();
+//         cy.contains('Deskripsi').click();
+//         cy.contains('Visi').click();
+//         cy.contains('Misi').click();
+//         cy.go('back');
+
+//         // open card Calon 2
+//         cy.get('a:contains("Lihat Profil Calon")').eq(1).click();
+//         cy.contains('Deskripsi').click();
+//         cy.contains('Visi').click();
+//         cy.contains('Misi').click();
+//         cy.go('back');
+
+//         // open card Calon 3
+//         cy.get('a:contains("Lihat Profil Calon")').eq(0).click();
+//         cy.contains('Deskripsi').click();
+//         cy.contains('Visi').click();
+//         cy.contains('Misi').click();
+//         cy.go('back');
+//     });
+
+//     it('successfully accessed Kilas Balik Pemilu 2019', () => {
+//         cy.contains('Kilas Balik Pemilu 2019');
+//         cy.get('a:contains("Klik untuk membaca")').eq(1).click();
+//         cy.contains('Presiden').click();
+//         cy.go('back');
+//     });
+// });
 
 // mengakses Partai Page dari Landing Page
-describe('Partai Page', () => {
-    it('successfully accessed Partai Page', async () => {        
-        cy.contains('Partai').click();
-        cy.url().should('include', '/partai-politik');
-        
-        // validasi kelengkapan partai (24 partai)
-        cy.contains('Partai Kebangkitan Nusantara (PKN)');
-        cy.contains('Partai Hati Nurani Rakyat (Hanura)');
-        cy.contains('Partai Demokrat');
-        cy.contains('Partai Kebangkitan Bangsa (PKB)');
-        cy.contains('Partai Demokrasi Indonesia Perjuangan (PDI-P)');
-        cy.contains('Partai Darul Aceh (PDA)');
-        cy.contains('Partai Gelombang Rakyat Indonesia (Gelora)');
-        cy.contains('Partai Persatuan Pembangunan (PPP)');
-        cy.contains('Partai Aceh');
-        cy.contains('Partai Adil Sejahtera Aceh (PAS Aceh)');
-        cy.contains('Partai Persatuan Indonesia (Perindo)');
-        cy.contains('Partai Bulan Bintang (PBB)');
-        cy.contains('Partai Soliditas Independent Rakyat Aceh (SIRA)');
-        cy.contains('Partai Garda Perubahan Indonesia (Garuda)');
-        cy.contains('Partai Nasional Demokrat (NasDem)');
-        cy.contains('Partai Gerakan Indonesia Raya (Gerindra)');
-        cy.contains('Partai Ummat');
-        cy.contains('Partai Nangroe Aceh (PNA)');
-        cy.contains('Partai Amanat Nasional (PAN)');
-        cy.contains('Partai Golongan Karya (Golkar)');
-        cy.contains('Partai Buruh');
-        cy.contains('Partai Keadilan Sejahtera (PKS)');
-        cy.contains('Partai Solidaritas Indonesia (PSI)');
-        cy.contains("Partai Generasi Atjeh Beusaboh Tha'at Dan Taqwa (Gabthat)");
-    });
+// describe('Partai Page', () => {
+//     beforeEach(() => {
+//         cy.viewport(1920, 1080);
+//         cy.visit('http://localhost:5173');
+//         cy.on('uncaught:exception', (err, runnable) => {
+//             console.error('Uncaught Exception:', err.message);
+//             return false;
+//             }
+//         );
 
-    // it('find data using the search bar', () => {
-    //     cy.get('input[placeholder="Search"]').type('demokrat{enter}');
-    // });
+//         cy.contains('Partai').click();
+//         cy.url().should('include', '/partai-politik');
+//     });
 
-    it('clicks all "View here" button', async () => {
-        const response = await fetch('http://localhost:5000/partaiInfos');
-        const partai = await response.json();
-        partai.map((element, index) => {
-            cy.get(`.card:nth-child(${index}) a[href^="/partai-politik/${element._id}"]`).click();
-        });
-    });
-});
+//     it('successfully accessed Partai Page', async () => {
+//         // validasi kelengkapan partai (24 partai)
+//         cy.contains('Partai Kebangkitan Nusantara (PKN)');
+//         cy.contains('Partai Hati Nurani Rakyat (Hanura)');
+//         cy.contains('Partai Demokrat');
+//         cy.contains('Partai Kebangkitan Bangsa (PKB)');
+//         cy.contains('Partai Demokrasi Indonesia Perjuangan (PDI-P)');
+//         cy.contains('Partai Darul Aceh (PDA)');
+//         cy.contains('Partai Gelombang Rakyat Indonesia (Gelora)');
+//         cy.contains('Partai Persatuan Pembangunan (PPP)');
+//         cy.contains('Partai Aceh');
+//         cy.contains('Partai Adil Sejahtera Aceh (PAS Aceh)');
+//         cy.contains('Partai Persatuan Indonesia (Perindo)');
+//         cy.contains('Partai Bulan Bintang (PBB)');
+//         cy.contains('Partai Soliditas Independent Rakyat Aceh (SIRA)');
+//         cy.contains('Partai Garda Perubahan Indonesia (Garuda)');
+//         cy.contains('Partai Nasional Demokrat (NasDem)');
+//         cy.contains('Partai Gerakan Indonesia Raya (Gerindra)');
+//         cy.contains('Partai Ummat');
+//         cy.contains('Partai Nangroe Aceh (PNA)');
+//         cy.contains('Partai Amanat Nasional (PAN)');
+//         cy.contains('Partai Golongan Karya (Golkar)');
+//         cy.contains('Partai Buruh');
+//         cy.contains('Partai Keadilan Sejahtera (PKS)');
+//         cy.contains('Partai Solidaritas Indonesia (PSI)');
+//         cy.contains("Partai Generasi Atjeh Beusaboh Tha'at Dan Taqwa (Gabthat)");
+//     });
 
-describe('Detail Partai Page', () => {
-    beforeEach(() => {
-        cy.visit('http://localhost:5173/partai-politik/');
-        cy.on('uncaught:exception', (err, runnable) => {
-            console.error('Uncaught Exception:', err.message);
-            return false;
-            }
-        );
-    });
+//     it('find partai using the search bar', () => {
+//         cy.get('input[placeholder="Search"]').type('demo{enter}');
+//         cy.wait(1000);
+//         cy.get('input[placeholder="Search"]').clear().type('hampura{enter}');
+//         cy.wait(1000);
+//     });
     
-    it('successfully accessed Detail Partai from buttons', () => {
-        cy.get('a:contains("View here")').eq(20).click()
+//     it('successfully accessed Detail Partai Page', async () => {
+//         const response = await fetch('http://localhost:5000/partaiInfos');
+//         const partai = await response.json();
+//         partai.map(({_id, website, ig_sosmed, fb_sosmed}) => {
+//             // clicks all "View here" button
+//             cy.get(`a[href^="/partai-politik/${_id}"]`).click()
+//             // akses isi Detail Partai Page
+//             cy.contains('Deskripsi').click()
+//             cy.contains('Tokoh').click()
+//             cy.contains('Fakta Unik').click()
+
+//             // cek social media partai
+//             if (website != ''){cy.contains(`${website}`)}
+//             if (ig_sosmed != ''){cy.contains(`${ig_sosmed}`)}
+//             if (fb_sosmed != ''){cy.contains(`${fb_sosmed}`)}
+//             cy.go('back')
+//         });
+//     });
+// });
+
+
+// akses Forum Page sebelum login
+describe('Forum Page', () => {
+    it('successfully accessed Forum Page', () => {
+        cy.contains('Forum').click();
+        cy.url().should('include', '/forum')
     });
 });
+
 
 
 // describe('Login Page', () => {
 //     it('should login by email', () => {
 //         cy.contains('Login').click();
+//         {
+//             auth: {
+//             username: 'wile',
+//             password: 'coyote',
+//           }
+//         }
 //     });
 // });
